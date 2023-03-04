69a279fbf90e1709b397b5500ca231b31d99c162
woodbyte <pedro.s.madeira@gmail.com>
2023-03-04 08:39:32 +0000

    Add compatibility with CharacterManager.

--- Patches/CompatibilityPatches.cs
+++ Patches/CompatibilityPatches.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using System.Reflection;
 
 namespace Bannerlord.AlwaysShowTitles.Patches
 {
@@ -9,15 +10,19 @@ namespace Bannerlord.AlwaysShowTitles.Patches
             var prefix = AccessTools.Method(typeof(CompatibilityPatches), nameof(CompatibilityPatches.Prefix));
             var postfix = AccessTools.Method(typeof(CompatibilityPatches), nameof(CompatibilityPatches.Postfix));
 
-            var addMethod = AccessTools.Method("NobleTitles.TitleBehavior:AddTitleToHero");
-            var removeMethod = AccessTools.Method("NobleTitles.TitleBehavior:RemoveTitleFromHero");
+            TryPatchMethod(harmony, "NobleTitles.TitleBehavior:AddTitleToHero", prefix, postfix);
+            TryPatchMethod(harmony, "NobleTitles.TitleBehavior:RemoveTitleFromHero", prefix, postfix);
 
-            if (addMethod != null && removeMethod != null)
-            {
-                harmony.Patch(addMethod, prefix: new HarmonyMethod(prefix), postfix: new HarmonyMethod(postfix));
-                harmony.Patch(removeMethod, prefix: new HarmonyMethod(prefix), postfix: new HarmonyMethod(postfix));
-            }
-
+            TryPatchMethod(harmony, "CharacterManager.GuiManager:ExecuteLoadCharacter", prefix, postfix);
+            TryPatchMethod(harmony, "CharacterManager.GuiManager:ExecuteSaveCharacter", prefix, postfix);
+            TryPatchMethod(harmony, "CharacterManager.ConsoleManager:SaveCharacter", prefix, postfix);
+            TryPatchMethod(harmony, "CharacterManager.ConsoleManager:LoadCharacter", prefix, postfix);
+            TryPatchMethod(harmony, "CharacterManager.ConsoleManager:LoadAllCompanions", prefix, postfix);
+            TryPatchMethod(harmony, "CharacterManager.ConsoleManager:PointReseter", prefix, postfix);
+            TryPatchMethod(harmony, "CharacterManager.ConsoleManager:PointReseterFull", prefix, postfix);
+            TryPatchMethod(harmony, "CharacterManager.ConsoleManager:GodCharacter", prefix, postfix);
+            TryPatchMethod(harmony, "CharacterManager.ConsoleManager:GodForAll", prefix, postfix);
+            TryPatchMethod(harmony, "CharacterManager.ConsoleManager:rename", prefix, postfix);
         }
 
         internal static void Prefix()
@@ -28,6 +33,19 @@ namespace Bannerlord.AlwaysShowTitles.Patches
         internal static void Postfix()
         {
             HeroPatches.EnableNamePatch = true;
+        }
+
+        internal static bool TryPatchMethod(Harmony harmony, string typeColonName, MethodInfo prefix, MethodInfo postfix)
+        {
+            var method = AccessTools.Method(typeColonName);
+
+            if (method != null)
+            {
+                harmony.Patch(method, prefix: new HarmonyMethod(prefix), postfix: new HarmonyMethod(postfix));
+                return true;
+            }
+
+            return false;
         }
     }
 }
